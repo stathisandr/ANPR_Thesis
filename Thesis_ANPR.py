47cc0d0ae754fe8ed05c9fb91c3bf98367b85ef6
stathisandr <andread.stathis@gmail.com>
2020-04-21 20:07:48 +0300

    Advanced ANPR with GUI

--- Thesis_ANPR.py
+++ Thesis_ANPR.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ANPRGRToll.ui'
+#
+# Created by: PyQt5 UI code generator 5.10.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QFileDialog
 import pyrebase
 from datetime import date
 from datetime import datetime
@@ -36,86 +46,6 @@ layer_names = net.getLayerNames()
 output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 colors = np.random.uniform(0, 255, size=(len(classes), 3))
 
-def vehicletype(customerID):
-    vehicletypestr = db.child("Users").child(customerID).child("vehicletype").get().val()
-    if vehicletype == "Light car":
-        return LightCar
-    elif vehicletype == "Motorcycle":
-        return Motorcycle
-    elif vehicletype == "4-axle truck":
-        return Truck_3
-    elif vehicletype == ">4-axle truck":
-        return Truck_4
-
-
-def datapost(LPtoString):
-    customer = db.child("Users").child(LPtoString).get().val()
-    if customer is None:
-        violation = {"licenceplate": LPtoString,"day": day,"time": datetime_ATH, "cost": "Unknown"}
-        db.child("Violations").push(violation, user['idToken'])
-    else:
-        CostString = vehicletype(customer)
-        purchase = {"day": day,"time": datetime_ATH, "cost": CostString}
-        db.child("Purchase").child(LPtoString).push(purchase)
-
-# Whitelist function first-half Licence Plate
-def removeFL(string):
-	bad_chars = ['C','D','F','G','J','L','Q','R','S','U','V','W']
-
-	for i in bad_chars:
-		string = string.replace(i,'')
-	return string
-
-def fourwheel(Image):
-    fw_width, fw_height = Image.size
-    im1 = Image.crop((0,0, fw_width/2, fw_height))
-    #print('im.size', im1.size)
-
-    im2 = Image.crop((fw_width/2, 0, fw_width, fw_height))
-    #print('im.size', im2.size)
-
-    # Use tesseract to convert image into string
-    first_half = pytesseract.image_to_string(im1, lang = 'grlp', config='--psm 9')
-    #print("First half :", first_half)
-    first_half = "".join(re.findall("[A-Z]+", first_half))
-    first_half = removeFL(first_half)
-
-    # Use tesseract to convert image into string
-    second_half = pytesseract.image_to_string(im2, lang = 'grlp', config='--psm 10')
-    #print("Second half :", second_half)
-    second_half = "".join(filter(lambda i: i.isdigit(), second_half))
-
-    LP = first_half+"-"+second_half
-    print("Licence Plate :"+ str(LP))
-    #print("--- %s seconds ---" %(time.time() - start_time))
-
-    datapost(LP)
-
-def twowheel(Image2):
-    tw_width, tw_height = Image2.size
-    im_top = Image2.crop((0,0, tw_width, tw_height/2))
-    #print('im.size', im1.size)
-
-    im_bot = Image2.crop((0, tw_height/2, tw_width, tw_height))
-    #print('im.size', im2.size)
-
-    # Use tesseract to convert image into string
-    top = pytesseract.image_to_string(im_top, lang = 'grlp', config='--psm 9')
-    print("Top :", top)
-    top = "".join(re.findall("[A-Z]+", top))
-    top = removeFL(top)
-
-    # Use tesseract to convert image into string
-    bottom = pytesseract.image_to_string(im_bot, lang = 'grlp', config='--psm 10')
-    print("Bottom :", bottom)
-    bottom = "".join(filter(lambda i: i.isdigit(), bottom))
-
-    LP = top+"-"+bottom
-    print("Licence Plate :"+ str(LP))
-    #print("--- %s seconds ---" %(time.time() - start_time))
-
-    datapost(LP)
-
 # Tesseract-OCR bin
 pytesseract.pytesseract.tesseract_cmd = r"/usr/bin/tesseract"
 
@@ -135,73 +65,263 @@ day = date.today().strftime("%b-%d-%Y")
 timezoneATH = pytz.timezone('Europe/Athens')
 datetime_ATH = datetime.now(timezoneATH).strftime("%H:%M:%S")
 
-#start_time = time.time()
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.setFixedSize(QtCore.QSize(790, 455))
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("../Downloads/auto_toll_gr_logo(1).png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        MainWindow.setWindowIcon(icon)
 
-# Loading image
-img = cv2.imread("cars/37.jpg")
-height, width, channels = img.shape
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
 
-# Detecting objects
-blob = cv2.dnn.blobFromImage(img, 0.00392, (480, 480), (0, 0, 0), True, crop=False)
+        self.frame = QtWidgets.QFrame(self.centralwidget)
+        self.frame.setGeometry(QtCore.QRect(10, 10, 331, 421))
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
 
-net.setInput(blob)
-outs = net.forward(output_layers)
+        self.originalphoto = QtWidgets.QLabel(self.frame)
+        self.originalphoto.setGeometry(QtCore.QRect(10, 20, 311, 341))
+        self.originalphoto.setScaledContents(True)
+        self.originalphoto.setObjectName("originalphoto")
 
-# Showing informations on the screen
-class_ids = []
-confidences = []
-boxes = []
-for out in outs:
-    for detection in out:
-        scores = detection[5:]
-        class_id = np.argmax(scores)
-        confidence = scores[class_id]
-        if confidence > 0.5:
-            # Object detected
-            center_x = int(detection[0] * width)
-            center_y = int(detection[1] * height)
-            w = int(detection[2] * width)
-            h = int(detection[3] * height)
+        self.selectimage = QtWidgets.QPushButton(self.frame)
+        self.selectimage.setGeometry(QtCore.QRect(10, 380, 151, 25))
+        self.selectimage.setObjectName("selectimage")
 
-            # Rectangle coordinates
-            x = int(center_x - w / 2)
-            y = int(center_y - h / 2)
+        self.anprimage = QtWidgets.QPushButton(self.frame)
+        self.anprimage.setGeometry(QtCore.QRect(168, 380, 151, 25))
+        self.anprimage.setObjectName("anprimage")
+        self.anprimage.setDisabled(True)
 
-            boxes.append([x, y, w, h])
-            confidences.append(float(confidence))
-            class_ids.append(class_id)
+        self.frame_2 = QtWidgets.QFrame(self.centralwidget)
+        self.frame_2.setGeometry(QtCore.QRect(360, 10, 421, 421))
+        self.frame_2.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame_2.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame_2.setObjectName("frame_2")
 
-indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
-print(indexes)
-font = cv2.FONT_HERSHEY_PLAIN
-for i in range(len(boxes)):
-    if i in indexes:
-        x, y, w, h = boxes[i]
-        label = str(classes[class_ids[i]])
-        color = colors[i]
-        cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
-        cv2.putText(img, "Licence Plate", (x, y), font, 1, color, 1)
+        self.carlicenceplate = QtWidgets.QLabel(self.frame_2)
+        self.carlicenceplate.setGeometry(QtCore.QRect(20, 20, 381, 131))
+        self.carlicenceplate.setScaledContents(True)
+        self.carlicenceplate.setObjectName("carlicenceplate")
 
-# Create new image
-prediction = img[y:y + h, x:x + w]
-# Save Licence Plate
-cv2.imwrite('/home/stathisandr/Desktop/CroppedImages/7.jpg',prediction)
+        self.bikelicenceplate = QtWidgets.QLabel(self.frame_2)
+        self.bikelicenceplate.setGeometry(QtCore.QRect(46, 26, 331, 251))
+        self.bikelicenceplate.setScaledContents(True)
+        self.bikelicenceplate.setObjectName("bikelicenceplate")
 
-Cropped_img = '/home/stathisandr/Desktop/CroppedImages/7.jpg'
-cv2.imshow("Cropped Image", cv2.imread(Cropped_img))
+        self.licenceplatetext = QtWidgets.QLineEdit(self.frame_2)
+        self.licenceplatetext.setGeometry(QtCore.QRect(120, 350, 181, 25))
+        self.licenceplatetext.setReadOnly(True)
+        self.licenceplatetext.setObjectName("licenceplatetext")
 
-im = Image.open(Cropped_img)
-im_width, im_height = im.size
-#print('im.size', im.size)
+        self.extravalidation = QtWidgets.QPlainTextEdit(self.frame_2)
+        self.extravalidation.setGeometry(QtCore.QRect(120, 321, 181, 20))
+        self.extravalidation.setStyleSheet("background-color: rgb(239, 236, 231); color: #ea0d0d")
+        self.extravalidation.setFrameShape(QtWidgets.QFrame.NoFrame)
+        self.extravalidation.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.extravalidation.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.extravalidation.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.extravalidation.setReadOnly(True)
+        self.extravalidation.setObjectName("extravalidation")
 
-if im_width>im_height and (im_width/im_height<0.8 or im_width/im_height>1.2):
-    # Rectangle
-    fourwheel(im)
-else:
-    # Square
-    twowheel(im)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
 
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+    
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Auto Licence Plate Recognition System"))
+        self.selectimage.setText(_translate("MainWindow", "Select Image"))
+        self.selectimage.clicked.connect(self.select_image)
+        self.anprimage.setText(_translate("MainWindow", "ANPR Image"))
+        self.anprimage.clicked.connect(self.anpr_image)
 
-cv2.imshow("Image", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    def select_image(self):
+        global path
+        filename = QFileDialog.getOpenFileName()
+        path = filename[0]
+        self.originalphoto.setPixmap(QtGui.QPixmap(path))
+        self.carlicenceplate.clear()
+        self.bikelicenceplate.clear()
+        self.licenceplatetext.clear()
+        self.extravalidation.clear()
+        self.anprimage.setDisabled(False)
+    
+    def anpr_image(self):
+        # Loading image
+        #print(path)
+        img = cv2.imread(path)
+        height, width, channels = img.shape
+        #print(height)
+        #print(width)
+
+        # Detecting objects
+        blob = cv2.dnn.blobFromImage(img, 0.00392, (480, 480), (0, 0, 0), True, crop=False)
+
+        net.setInput(blob)
+        outs = net.forward(output_layers)
+
+        # Showing informations on the screen
+        class_ids = []
+        confidences = []
+        boxes = []
+        for out in outs:
+            for detection in out:
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+                if confidence > 0.5:
+                    # Object detected
+                    center_x = int(detection[0] * width)
+                    center_y = int(detection[1] * height)
+                    w = int(detection[2] * width)
+                    h = int(detection[3] * height)
+
+                    # Rectangle coordinates
+                    x = int(center_x - w / 2)
+                    y = int(center_y - h / 2)
+
+                    boxes.append([x, y, w, h])
+                    confidences.append(float(confidence))
+                    class_ids.append(class_id)
+
+        indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+        #print(indexes)
+        font = cv2.FONT_HERSHEY_PLAIN
+        for i in range(len(boxes)):
+            if i in indexes:
+                x, y, w, h = boxes[i]
+                label = str(classes[class_ids[i]])
+                color = colors[i]
+                cv2.rectangle(img, (x, y), (x + w, y + h), color, 4)
+                cv2.putText(img, "Licence Plate", (x, y), font, 3, color, 3)
+
+        try:
+            # Create new image
+            prediction = img[y:y+h,x:x+w]
+        except:
+            message_error ="No licence plate found!"
+            self.licenceplatetext.setText(message_error)
+            
+
+        # Save Licence Plate
+        cv2.imwrite('7.jpg',prediction)
+
+        Cropped_img = '7.jpg'
+        #cv2.imshow("Cropped Image", cv2.imread(Cropped_img))
+
+        im = Image.open(Cropped_img)
+        im_width, im_height = im.size
+        #print('im.size', im.size)
+
+        if im_width>im_height and (im_width/im_height<0.8 or im_width/im_height>1.2):
+            # Rectangle
+            self.carlicenceplate.setPixmap(QtGui.QPixmap(Cropped_img))
+            fourwheel(self,im)
+        else:
+            # Square
+            self.bikelicenceplate.setPixmap(QtGui.QPixmap(Cropped_img))
+            twowheel(self,im)
+  
+def vehicletype(customerID):
+    vehicletypestr = db.child("Users").child(customerID).child("vehicletype").get().val()
+    if vehicletype == "Light car":
+        return LightCar
+    elif vehicletype == "Motorcycle":
+        return Motorcycle
+    elif vehicletype == "4-axle truck":
+        return Truck_3
+    elif vehicletype == ">4-axle truck":
+        return Truck_4
+
+def datapost(self, LPtoString):
+    customer = db.child("Users").child(LPtoString).get().val()
+
+    self.licenceplatetext.setText(LPtoString)
+
+    if customer is None:
+        violation = {"licenceplate": LPtoString,"day": day,"time": datetime_ATH, "cost": "Unknown"}
+        db.child("Violations").push(violation, user['idToken'])
+    else:
+        CostString = vehicletype(customer)
+        purchase = {"day": day,"time": datetime_ATH, "cost": CostString}
+        db.child("Purchase").child(LPtoString).push(purchase)
+
+# Whitelist function first-half Licence Plate
+def removeFL(string):
+	bad_chars = ['C','D','F','G','J','L','Q','R','S','U','V','W']
+
+	for i in bad_chars:
+		string = string.replace(i,'')
+	return string
+
+def fourwheel(self,Image):
+    fw_width, fw_height = Image.size
+    im1 = Image.crop((0,0, fw_width/2, fw_height))
+    #print('im.size', im1.size)
+
+    im2 = Image.crop((fw_width/2, 0, fw_width, fw_height))
+    #print('im.size', im2.size)
+
+    # Use tesseract to convert image into string
+    first_half = pytesseract.image_to_string(im1, lang = 'grlp', config='--psm 9')
+    #print("First half :", first_half)
+    first_half = "".join(re.findall("[A-Z]+", first_half))
+    first_half = removeFL(first_half)
+
+    # Use tesseract to convert image into string
+    second_half = pytesseract.image_to_string(im2, lang = 'grlp', config='--psm 10')
+    #print("Second half :", second_half)
+    second_half = "".join(filter(lambda i: i.isdigit(), second_half))
+
+    LP = first_half+"-"+second_half
+
+    if len(LP)!=8:
+        self.extravalidation.setPlainText("Needs further validation")
+    #print("Licence Plate :"+ str(LP))
+    #print("--- %s seconds ---" %(time.time() - start_time))
+
+    datapost(self,LP)
+
+def twowheel(self, Image2):
+    tw_width, tw_height = Image2.size
+    im_top = Image2.crop((0,0, tw_width, tw_height/2))
+    #print('im.size', im1.size)
+
+    im_bot = Image2.crop((0, tw_height/2, tw_width, tw_height))
+    #print('im.size', im2.size)
+
+    # Use tesseract to convert image into string
+    top = pytesseract.image_to_string(im_top, lang = 'grlp', config='--psm 10')
+    #print("Top :", top)
+    top = "".join(re.findall("[A-Z]+", top))
+    top = removeFL(top)
+
+    # Use tesseract to convert image into string
+    bottom = pytesseract.image_to_string(im_bot, lang = 'grlp', config='--psm 10')
+    #print("Bottom :", bottom)
+    bottom = "".join(filter(lambda i: i.isdigit(), bottom))
+
+    LP = top+"-"+bottom
+    if len(LP)!=7:
+        self.extravalidation.setPlainText("Needs further validation")
+    #print("Licence Plate :"+ str(LP))
+    #print("--- %s seconds ---" %(time.time() - start_time))
+
+    datapost(self,LP)
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
